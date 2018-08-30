1e0ea4f043b605db8cd7d8afad5ab1aea738f1f7
burntcustard <burntcustard@gmail.com>
2018-08-31 00:15:27 +0100

    Initial bot-containing commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # burnt-mc-bot
+
 A bot to help out in the mc.burnt.io Discord
+
+Many thanks to this Discord bot guide: https://anidiots.guide/
---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(config.prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
+
+client.login(config.token);
