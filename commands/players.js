518e6273b616d4a50ed40b9f9ed4082cd450ced0
Reece Bennett <reece.bennett1@gmail.com>
2018-09-04 13:08:28 +0100

    Update player list to include avatars

--- commands/players.js
+++ commands/players.js
@@ -1,5 +1,6 @@
-
 const BaseCommand = require('./base');
+const config = require("../config.json");
+const Discord = require("discord.js");
 const getMCJSON = require("./../mcsrvstat");
 
 module.exports = class Command extends BaseCommand {
@@ -9,11 +10,16 @@ module.exports = class Command extends BaseCommand {
         this.desc = "Players command";
     }
 
+    /**
+     * @param {Discord.Message} message 
+     */
     run(message) {
+        const guild = message.guild;
+
         return new Promise((resolve, reject) => {
             getMCJSON()
             .then((json) => {
-                let playerStr = "test";
+                let playerStr = "";
                 let players = json.players;
                 if (json.offline) {
                     playerStr = "Server is offline!";
@@ -22,14 +28,33 @@ module.exports = class Command extends BaseCommand {
                     playerStr = "No one is playing :(";
                 } else
                 if (players && players.list.length > 0) {
-                    playerStr = "Currently online: ";
-                    playerStr += players.list.join(", ");
+                    // Used if the emoji needs to be created (cannot use straight away)
+                    const steve_emoji = guild.emojis.find(e => e.name == "steve");
+
+                    players.list.forEach((player, i, a) => {
+                        let emoji = guild.emojis.find(e => e.name == player);
+                        
+                        if (!emoji) {
+                            guild.createEmoji(`https://minotar.net/helm/${player}`, player)
+                                .then(e => {
+                                    emoji = e;
+                                    console.log(`Emoji :${emoji.name}: created`);
+                                }).catch(console.error);
+                        } else {
+                            console.log(`Emoji :${emoji.name}: already exists`);
+                        }
+
+                        a[i] = `    ${emoji ? emoji : steve_emoji} \`${a[i]}\``
+                    });
+
+                    playerStr = `**Currently playing on ${config.mcserver}:**\n\n`;
+                    playerStr += players.list.join("\n\n");
                 }
                 message.channel.send(playerStr);
                 resolve(playerStr);
             })
             .catch((error) => {
-                console.log("Error handling server info");
+                console.error(error);
             });
         });
     }
