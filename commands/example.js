29fd7bb093eb36007b34e9e6d4cb8b3c1f9a71aa
burntcustard <burntcustard@gmail.com>
2018-09-05 09:20:59 +0100

    A bit more explanation in example command

--- commands/example.js
+++ commands/example.js
@@ -4,15 +4,21 @@ const BaseCommand = require('./base');
 module.exports = class Command extends BaseCommand {
 
     constructor() {
+
+        // Call the base command constructor to inherit its methods
         super();
+
+        // A description of this command that explains what it does
         this.desc = "An example command to test with";
+
+        // Sets of keywords that must all appear in a message to active this
         this.aliases = [
-            ["!foo"],
-            ["example", "command"],
-            ["!ping"]
+            ["the", "example", "command"],
+            ["alternative", "key", "words"]
         ];
     }
 
+    // What happens when the command is activated
     run(message) {
         message.channel.send("pong!");
     }
